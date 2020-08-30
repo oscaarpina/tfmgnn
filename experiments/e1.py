6e77d341603706da564064eee6e6d6a808d3270e
Oscar Pina <oscarpb97@gmail.com>
2020-08-30 19:37:30 +0200

    reestructure the prj

---
+++ experiments/e1.py
@@ -0,0 +1,6 @@
+from adgnn.data.pipeline import *
+from adgnn.data.feature import *
+from adgnn.data.gconversor import GSparseConversor
+from adgnn.dl.metrics import *
+from adgnn.dl.trainer import Trainer
+from adgnn.models.aal_model import AALModel
